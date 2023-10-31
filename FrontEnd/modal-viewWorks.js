9cdf42f9fbf44ad0d131f6d1659170e449a593d8
araba <araba.mohamed@gmail.com>
2023-10-31 12:51:04 +0100

    mies en place d la modale addWork

--- FrontEnd/modal-viewWorks.js
+++ FrontEnd/modal-viewWorks.js
@@ -1,55 +1,56 @@
-const focusableSelector = 'button';
+const focusableSelector = 'button, input, select';
 let modal = null;
 let focusables = [];
 let previouslyFocusedElement = null;
 
-const deleteFigureFromApi = async function (id) {
-  debugger;
-  await fetch(`http://localhost:5678/api/works/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + localStorage.getItem('token'),
-    },
-  });
-  // const responseJson = await response.json();
-  // console.log(responseJson);
-};
-const addEventListnerToButtonTrash = async (e) => {
+const deleteFigureFromApi = async (e) => {
   e.preventDefault();
-  const id = e.target.parentNode.getAttribute('data-id');
+  // debugger;
+  const id = e.target.closest('figure').dataset.id;
   const confirmation = confirm('Voulez vous supprimer ce projet ?');
   if (confirmation) {
-    await deleteFigureFromApi(id);
-    console.log('figure modal', e.target.closest('figure'));
-    e.target.parentNode.remove();
-    console.log(
-      'figure gallery',
-      document.querySelector(`.gallery figure[data-id="${id}"]`)
-    );
-    document.querySelector(`.gallery figure[data-id="${id}"]`).remove();
+    const token = localStorage.getItem('token');
+    fetch(`http://localhost:5678/api/works/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new error('La supression du travai à echoué.');
+        }
+        e.target.closest('figure').remove();
+        document.querySelector(`.gallery figure[data-id="${id}"]`).remove();
+        document.querySelector('.modal-gallery figure[data-id="2"]').focus();
+      })
+      .catch((error) => console.error(error));
   }
 };
-function addElementToModal(modalGallery, id, imageUrl, title) {
+
+function addFigureToModal(id, imageUrl, title) {
   const figure = document.createElement('figure');
   figure.style.position = 'relative';
   figure.setAttribute('data-id', id);
   const img = document.createElement('img');
   const buttonTrash = document.createElement('button');
-  buttonTrash.classList.add('material-symbols-outlined');
   buttonTrash.classList.add('trash');
-  // buttonTrash.classList.add('data-id');
-  // buttonTrash.setAttribute('data-id', id);
   buttonTrash.setAttribute('type', 'button');
-  buttonTrash.textContent = 'delete';
   buttonTrash.style.cursor = 'pointer';
+  const iFontAwesome = document.createElement('i');
+  iFontAwesome.className = 'fa-solid fa-trash-can fa-lg';
+  buttonTrash.appendChild(iFontAwesome);
 
   img.src = imageUrl;
   img.alt = title;
 
   figure?.appendChild(img);
   figure?.appendChild(buttonTrash);
-  modalGallery?.appendChild(figure);
+  // Rajout d'un EventListener sur chaque bouton poubelle
+  buttonTrash.addEventListener('click', deleteFigureFromApi);
+
+  return figure;
 }
 
 const openModal = async function (e) {
@@ -61,15 +62,16 @@ const openModal = async function (e) {
   // récup des projets via api
   const res = await fetch('http://localhost:5678/api/works');
   const works = await res.json();
-  // Suppression si besoins puis chargement des projets au demarage de la modale
+  // Suppression puis chargement des projets au demarage de la modale
   const figures = modal.querySelectorAll('.modal-gallery figure');
   figures.forEach((figure) => {
     figure.remove();
   });
   const modalGallery = modal.querySelector('.modal-gallery');
-  works.map((work) =>
-    addElementToModal(modalGallery, work.id, work.imageUrl, work.title)
-  );
+  works.map((work) => {
+    const figure = addFigureToModal(work.id, work.imageUrl, work.title);
+    modalGallery?.appendChild(figure);
+  });
 
   //gestion de l'element selectionné via le curseur
   focusables = Array.from(modal.querySelectorAll(focusableSelector));
@@ -79,38 +81,16 @@ const openModal = async function (e) {
   modal.removeAttribute('aria-hidden');
   modal.setAttribute('aria-modal', 'true');
   modal.addEventListener('click', closeModal);
-  modal.querySelector('.crossButton').addEventListener('click', closeModal);
+  modal
+    .querySelectorAll('.crossButton')
+    .forEach((crossButton) =>
+      crossButton.addEventListener('click', closeModal)
+    );
   modal
     .querySelector('.js-modal-stop')
     .addEventListener('click', stopPropagation);
-  // Rajout d'un EventListener sur chaque bouton poubelle
-  modal
-    .querySelectorAll('.trash')
-    .forEach((button) =>
-      button.addEventListener('click', addEventListnerToButtonTrash)
-    );
-
-  // modal
-  //   .querySelector('form .buttonAddWork')
-  //   .addEventListener('click', callApiDeleteWork);
 };
 
-// const callApiDeleteWork = async (e) => {
-//   e.preventDefault();
-//   debugger;
-//   if (confirm('Voulez vous supprimer ce projet ?')) {
-//     const res = await fetch('http://localhost:5678/api/works/11', {
-//       method: 'DELETE',
-//       headers: {
-//         ContentType: 'application/json',
-//         Authorization: 'Bearer ' + localStorage.getItem('token'),
-//       },
-//     });
-//     if (res.ok) {
-//       alert('Projet 11 supprimé');
-//     }
-//   }
-// };
 const closeModal = function (e) {
   if (modal === null) return;
   if (previouslyFocusedElement !== null) previouslyFocusedElement.focus();
@@ -118,8 +98,11 @@ const closeModal = function (e) {
   modal.setAttribute('aria-hidden', 'true');
   modal.removeAttribute('aria-modal');
   modal.removeEventListener('click', closeModal);
-  const crossbutton = document.querySelector('.crossButton');
-  crossbutton.removeEventListener('click', closeModal);
+  modal
+    .querySelectorAll('.crossButton')
+    .forEach((crossButton) =>
+      crossButton.removeEventListener('click', closeModal)
+    );
   modal
     .querySelector('.js-modal-stop')
     .removeEventListener('click', stopPropagation);
@@ -129,11 +112,6 @@ const closeModal = function (e) {
     modal = null;
   };
   modal.addEventListener('animationend', hideModal);
-  modal
-    .querySelectorAll('.trash')
-    .forEach((button) =>
-      button.removeEventListener('click', addEventListnerToButtonTrash)
-    );
 };
 
 const stopPropagation = function (e) {
@@ -169,3 +147,34 @@ window.addEventListener('keydown', function (e) {
     focusInModal(e);
   }
 });
+
+document
+  .querySelector('.modal-viewWorks .buttonAddWork')
+  .addEventListener('click', async (e) => {
+    e.preventDefault();
+    document.querySelector('.modal-viewWorks').style.display = 'none';
+    document.querySelector('.modal-addWork').style.display = 'flex';
+
+    const selectCategory = document.getElementById('modal-category');
+    if (Array.from(selectCategory).length === 1) {
+      await fetch('http://localhost:5678/api/categories')
+        .then((response) => response.json())
+        .then((data) => {
+          data.forEach((category) => {
+            const categoryOption = document.createElement('option');
+            categoryOption.setAttribute('value', category.id);
+            categoryOption.textContent = category.name;
+
+            selectCategory.appendChild(categoryOption);
+          });
+        });
+    }
+  });
+
+document
+  .querySelector('.modal-addWork .arrowButton')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.modal-addWork').style.display = 'none';
+    document.querySelector('.modal-viewWorks').style.display = 'flex';
+  });
